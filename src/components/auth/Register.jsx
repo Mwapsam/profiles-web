c8366d42752f097587cf4f574ee901d4a67ea526
Mwapsam <mwapsam@gmail.com>
2022-09-23 09:59:36 +0200

    Implementent auth

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import 'react-phone-number-input/style.css';
 import PhoneInput from 'react-phone-number-input';
 import { useCreateUserMutation } from '../../redux/users/userSlice';
@@ -10,9 +11,13 @@ const Register = () => {
   const [createUser] = useCreateUserMutation();
   const [user, setUser] = useState(initialState);
   const [value, setValue] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   const registerUser = (e) => {
     e.preventDefault();
+    setIsLoading(true);
     createUser({
       firstname: user.firstname,
       lastname: user.lastname,
@@ -20,6 +25,8 @@ const Register = () => {
       email: user.email,
       password: user.password,
     });
+    setIsLoading(false);
+    navigate('/email-verification');
     setUser('');
   };
 
@@ -81,14 +88,25 @@ const Register = () => {
                 </div>
 
                 <div>
-                  <button type="submit" className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                      <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
-                      </svg>
-                    </span>
-                    Sign in
-                  </button>
+                  {isLoading ? (
+                    <button type="submit" className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" disabled>
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                        <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
+                        </svg>
+                      </span>
+                      Sign in
+                    </button>
+                  ) : (
+                    <button type="submit" className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                        <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
+                        </svg>
+                      </span>
+                      Sign in
+                    </button>
+                  )}
                 </div>
               </form>
             </div>
