b2086b29a967ea403297539ae51cddf3ddf7ef91
Mwapsam <mwapsam@gmail.com>
2022-09-23 09:57:48 +0200

    Create user component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
 import { userSlice } from './redux/users/userSlice';
 import './index.css';
@@ -8,8 +9,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ApiProvider api={userSlice}>
-      <App />
-    </ApiProvider>
+    <BrowserRouter>
+      <ApiProvider api={userSlice}>
+        <App />
+      </ApiProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
